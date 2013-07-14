59b6476fe7a65085482f6a83f3d13098032e6382
Faisal Nahian <faisalnahian@hotmail.com>
2013-07-14 14:42:45 -0400

    Version 1.1.0 Update 07.14.13
    > Added the copy & paste functionality for all job board sites
    (indicated with a red border)
    > Reduced the wrapper height
    > UI improvements.
    > Wording and UI changes in options menu.

--- content.js
+++ content.js
@@ -13,7 +13,6 @@ if (localDebug) {
 var loginURL = domain + "login.php";
 var addJobURL = domain + "addJob.php";
 var homeURL = domain + "home.php";
-var newEntryURL = "http://appliedat.com/new-entry.php";
 
 var haveLogins = false,
 User = '',
@@ -24,8 +23,10 @@ Pass = '';
 function CreateBox() {
   //evt_Name, evt_Date, evt_Where, evt_OtherDetails, evt_List
   var el = document.createElement('div');
-  var s = "<div class='header'>Applied.at<span id='close' style='font-size: 17px;font-weight: bold; color#333;right: 10px;position: absolute;'>X</span></div><div class='main'><div class='login' style='display:none'><br><div class='row'><span>Please update your Appliedat.com credentials to Save jobs in your account. <br>If not signed up, please signup here <a href='http://appliedat.com/signup.php' target='_blank'>Signup</a></span><br><br></div><div class='row'><span>UserName</span><br /><input type='text' id='appliedat_username' /></div><div class='row'><span>Password</span><br /><input type='password' id='appliedat_password' /></div><div class='row' style='display:none' id='saved_info'><span style='color:blue;' >Your account details have been saved successfully. <br>You can start using the extension now to save job listings..</span></div><a id='btnSaveLogin' class='appliedat_button'>Update</a></div><div class='content'><div class='row'><span>Title</span><br /><input type='text' id='appliedat_SiteName' /></div><div class='row'><span>Saved Date &amp; Time</span><br /><input type='text' id='appliedat_SaveDate' /></div><div class='row'><span>URL</span><br /><input type='text' id='appliedat_Url' /></div><div class='row'><span>Job Description</span><br /><textarea id='appliedat_Description'></textarea></div><div class='row'><span>Personal Note</span><br /><input type='text' id='appliedat_Notes' /></div><div class='row'><span>Did you apply for this job?</span> &nbsp;&nbsp; <input type='radio' name='applied' value='1' />Applied &nbsp;&nbsp; <input type='radio' name='applied' value='0' checked='checked' />Didn&#39;t Apply</div><div class='row reminder' style='display:none'><div><input type='checkbox' checked='checked' />&nbsp;&nbsp; Remind me to follow-up when? (Y/m/d format) <input type='text' id='appliedat_reminderDate' /></div></div><a id='btnSaveJob' class='appliedat_button'>Save</a></div></div><div class='footer'><a href='http://appliedat.com/home.php' target='_blank'>Recently Saved&nbsp;&nbsp;/</a><a href='http://appliedat.com/applied.php' target='_blank'>Applied Jobs&nbsp;&nbsp;/</a><a href='http://appliedat.com/help.php' target='_blank'>Help</a>";
+  var s = "<div class='header'>Add to Applied.at<span id='close' style='font-size: 17px;font-weight: bold; color#333;right: 10px;position: absolute;'>X</span></div><div class='main'><div class='login' style='display:none'><br><div class='row'><span>Please update your Appliedat.com credentials to Save jobs in your account. <br>If not signed up, please signup here <a href='http://appliedat.com/signup.php' target='_blank'>Sign up</a></span><br><br></div><div class='row'><span>Username</span><br /><input type='text' id='appliedat_username' /></div><div class='row'><span>Password</span><br /><input type='password' id='appliedat_password' /></div><div class='row' style='display:none' id='saved_info'><span style='color:#0099CC;'>Your account details have been saved successfully. <br>You can start using the extension now to save job listings..</span></div><a id='btnSaveLogin' class='appliedat_button'>Update</a></div><div class='content'><div class='row'><span>Title</span><br /><input type='text' id='appliedat_SiteName' /></div><div class='row'><span>Saved Date &amp; Time</span><br /><input type='text' id='appliedat_SaveDate' /></div><div class='row'><span>URL</span><br /><input type='text' id='appliedat_Url' /></div><div class='row'><span>Job Description</span><br /><textarea id='appliedat_Description'></textarea></div><div class='row'><span>Personal Note</span><br /><input type='text' id='appliedat_Notes' /></div><div class='row'><span>Did you apply for this job?</span> &nbsp;&nbsp; <input type='radio' name='applied' value='1' />Applied &nbsp;&nbsp; <input type='radio' name='applied' value='0' checked='checked' />Didn&#39;t Apply</div><div class='row reminder' style='display:none'><div><input type='checkbox' checked='checked' />&nbsp;&nbsp; Remind me to follow-up when? (Y/m/d format) <input type='text' id='appliedat_reminderDate' /></div></div><a id='btnSaveJob' class='appliedat_button'>Save</a></div></div><div class='footer'><a href='http://appliedat.com/home.php' target='_blank'>Recently Saved&nbsp;&nbsp;/</a><a href='http://appliedat.com/applied.php' target='_blank'>Applied Jobs&nbsp;&nbsp;/</a><a href='http://appliedat.com/help.php' target='_blank'>Help</a>";
   el.setAttribute("id", "appliedat-wrapper");
+  el.setAttribute("style", "display:none");
+  
   document.body.appendChild(el);
   el.innerHTML = s;
   document.querySelectorAll('#appliedat-wrapper input[type=radio]')[0].onclick = function () { //Applied clicked
@@ -248,22 +249,23 @@ chrome.extension.onMessage.addListener(
       }
 
     }
-    if (isjobSite) {
-      var d = new Date();
-      document.getElementById('appliedat_SiteName').value = document.title;
-      document.getElementById('appliedat_SaveDate').value = d.toGMTString();
-      document.getElementById('appliedat_Url').value = location.href;
-      document.getElementById('appliedat_Description').value = description;
-      document.getElementById('appliedat_Notes').value = '';
-      var reminder = d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate();
-      //;+ " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-      document.getElementById('appliedat_reminderDate').value = reminder;
-      document.getElementById('appliedat-wrapper').style.display = 'block';
-    } else {
-      chrome.runtime.sendMessage({
-        type : "openTab",
-        tab : newEntryURL
-      });
-    }
+  if (!isjobSite) {
+     // do extra magic to tell user we don't support this yet
+      document.querySelector('#appliedat-wrapper .header').setAttribute('style', 'border-width: 3px; border-color:red; border-bottom: 1px solid #C3D4DB');
+      document.querySelector('#appliedat-wrapper .main').setAttribute('style', 'border-width: 3px; border-color:red;');
+      document.querySelector('#appliedat-wrapper .footer').setAttribute('style', 'border-width: 3px; border-color:red; border-top: 1px solid #C3D4DB');
+      document.getElementById('appliedat_Description').style.color='red';
+      description = "We don't support this site yet, so ENTER (copy and paste) the description on your own......"; 
+    } 
+    var d = new Date();
+    document.getElementById('appliedat_SiteName').value = document.title;
+    document.getElementById('appliedat_SaveDate').value = d.toGMTString();
+    document.getElementById('appliedat_Url').value = location.href;
+    document.getElementById('appliedat_Description').value = description;
+    document.getElementById('appliedat_Notes').value = '';
+    var reminder = d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate();
+    //;+ " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+    document.getElementById('appliedat_reminderDate').value = reminder;
+    document.getElementById('appliedat-wrapper').style.display = 'block';
   }
 });
