11637283282b0b4e0a00b0edefbfe333306384ac
Faisal Nahian <faisalnahian@hotmail.com>
2015-03-28 22:07:56 -0400

    Sites compatibility fix
    Fixed job descriptions on
    
    craiglist.org
    linkedin.com
    theladders.com
    
    Added compatibility with http://www.careerbuilder.com/

--- content.js
+++ content.js
@@ -6,7 +6,7 @@ http://mylocalhost/Appliedat/login.php
 var localDebug = false;
 var domain = '';
 if (localDebug) {
-  domain = "http://mylocalhost/Appliedat/";
+  domain = "http://appliedat.localhost/";
 } else {
   domain = "http://appliedat.com/";
 }
@@ -17,8 +17,8 @@ var homeURL = domain + "home.php";
 var haveLogins = false,
 User = '',
 Pass = '';
-
-
+function sel(sel, el) {    return (el || document).querySelector(sel); }
+function selAll(sel, el) { return (el || document).querySelectorAll(sel); }
 
 function CreateBox() {
   //evt_Name, evt_Date, evt_Where, evt_OtherDetails, evt_List
@@ -30,19 +30,19 @@ function CreateBox() {
   document.body.appendChild(el);
   el.innerHTML = s;
   document.querySelectorAll('#appliedat-wrapper input[type=radio]')[0].onclick = function () { //Applied clicked
-    document.querySelector('#appliedat-wrapper .reminder').style.display = 'block';
-    //document.querySelector('#appliedat-wrapper').style.height = '670px';
+    sel('#appliedat-wrapper .reminder').style.display = 'block';
+    //sel('#appliedat-wrapper').style.height = '670px';
   }
   document.querySelectorAll('#appliedat-wrapper input[type=radio]')[1].onclick = function () { //Not Applied clicked
-    document.querySelector('#appliedat-wrapper .reminder').style.display = 'none';
-    //document.querySelector('#appliedat-wrapper').style.height = '600px';
-    document.querySelector('#appliedat-wrapper #appliedat_reminderDate').checked = false;
+    sel('#appliedat-wrapper .reminder').style.display = 'none';
+    //sel('#appliedat-wrapper').style.height = '600px';
+    sel('#appliedat-wrapper #appliedat_reminderDate').checked = false;
 
   }
-  document.querySelector('#appliedat-wrapper #close').onclick = function () {
+  sel('#appliedat-wrapper #close').onclick = function () {
     document.getElementById('appliedat-wrapper').style.display = 'none';
   }
-  document.querySelector('#appliedat-wrapper #btnSaveLogin').onclick = function () {
+  sel('#appliedat-wrapper #btnSaveLogin').onclick = function () {
     if(document.getElementById('appliedat_username').value!='' && document.getElementById('appliedat_password').value!=''  ){
       chrome.runtime.sendMessage({
         type : "saveLogins",
@@ -52,16 +52,16 @@ function CreateBox() {
         haveLogins = response.haveLogins;
         User = response.User;
         Pass = response.Pass;
-        document.querySelector('#appliedat-wrapper #saved_info').style.display='block';
-         document.querySelector('#appliedat-wrapper #btnSaveLogin').style.display='none';
+        sel('#appliedat-wrapper #saved_info').style.display='block';
+         sel('#appliedat-wrapper #btnSaveLogin').style.display='none';
         setTimeout(function(){
-          document.querySelector('#appliedat-wrapper .main .login').style.display='none';
-          document.querySelector('#appliedat-wrapper .main .content').style.display='block';
+          sel('#appliedat-wrapper .main .login').style.display='none';
+          sel('#appliedat-wrapper .main .content').style.display='block';
         },1500);  
       });
       }
   }
-  document.querySelector('#appliedat-wrapper #btnSaveJob').onclick = function () {
+  sel('#appliedat-wrapper #btnSaveJob').onclick = function () {
       var xmlhttp = new XMLHttpRequest;
       var authUrl = addJobURL;
       var applied = (document.querySelectorAll('#appliedat-wrapper input[type=radio]')[0].checked ? '1' : '0');
@@ -97,8 +97,8 @@ chrome.runtime.sendMessage({
   Pass = response.Pass;
   CreateBox();
   if (!haveLogins) {
-      document.querySelector('#appliedat-wrapper .main .login').style.display='block';
-      document.querySelector('#appliedat-wrapper .main .content').style.display='none';
+      sel('#appliedat-wrapper .main .login').style.display='block';
+      sel('#appliedat-wrapper .main .content').style.display='none';
     }
 });
 chrome.extension.onMessage.addListener(
@@ -109,10 +109,10 @@ chrome.extension.onMessage.addListener(
     var longLine = '\n---------------------------------------------------------------------------------------\n';
     // craiglist.org
     if (document.domain.indexOf('craigslist.org') != -1) {
-      if (document.querySelector('#postingbody') && document.querySelector('.cltags')) {
+      if (sel('#postingbody') && sel('.notices')) {
         isjobSite = true;
-        description = document.querySelector('#postingbody').innerText.trim() + longLine +
-          'TAGS:-\n' + document.querySelector('.cltags').innerText.trim();
+        description = sel('#postingbody').innerText.trim() + longLine +
+          'TAGS:-\n' + sel('.notices').innerText.trim();
       }
     }
     // idealist.org
@@ -127,28 +127,28 @@ chrome.extension.onMessage.addListener(
       }
     }
     // indeed.com
-    if (document.domain.indexOf('indeed.com') != -1) {
-      if (document.querySelector('#job_header') && document.querySelector('#job_header+table')) {
+    if (document.domain.indexOf('indeed.c') != -1) {
+      if (sel('#job_header') && sel('#job_header+table')) {
         isjobSite = true;
-        description = document.querySelector('#job_header').innerText.trim() + longLine +
-          document.querySelector('#job_header+table').innerText.trim();
+        description = sel('#job_header').innerText.trim() + longLine +
+          sel('#job_header+table').innerText.trim();
       }
     }
     // USAJOBS.org TODO Need to Grab the JobTitle in the header
     if (document.domain.indexOf('usajobs.gov') != -1) {
-      if (document.querySelector('#jobinfo1') && document.querySelector('#jobinfo2') &&
-        document.querySelector('#jobsummary') && document.querySelector('#keyrequirements') && 
-        document.querySelector('#duties') && document.querySelector('#qualifications') &&
-        document.querySelector('#requireddocuments') && document.querySelector('#agencycontact')) {
+      if (sel('#jobinfo1') && sel('#jobinfo2') &&
+        sel('#jobsummary') && sel('#keyrequirements') && 
+        sel('#duties') && sel('#qualifications') &&
+        sel('#requireddocuments') && sel('#agencycontact')) {
         isjobSite = true;
-        description = document.querySelector('#jobinfo1').innerText.trim() + longLine +
-          document.querySelector('#jobinfo2').innerText.trim() + longLine +
-          document.querySelector('#jobsummary').innerText.trim() + longLine +
-          document.querySelector('#keyrequirements').innerText.trim() + longLine +
-          document.querySelector('#duties').innerText.trim() + longLine +
-          document.querySelector('#qualifications').innerText.trim() + longLine +
-          document.querySelector('#requireddocuments').innerText.trim() + longLine +
-          document.querySelector('#agencycontact').innerText.trim();
+        description = sel('#jobinfo1').innerText.trim() + longLine +
+          sel('#jobinfo2').innerText.trim() + longLine +
+          sel('#jobsummary').innerText.trim() + longLine +
+          sel('#keyrequirements').innerText.trim() + longLine +
+          sel('#duties').innerText.trim() + longLine +
+          sel('#qualifications').innerText.trim() + longLine +
+          sel('#requireddocuments').innerText.trim() + longLine +
+          sel('#agencycontact').innerText.trim();
       }
     }
     // Monster.com TODO Add more support
@@ -161,70 +161,81 @@ chrome.extension.onMessage.addListener(
           document.getElementById('jobsummary').innerText.trim();
       }
 
-      if (document.querySelector('table')) {
+      if (sel('table')) {
         isjobSite = true;
-        description = document.querySelector('table').innerText.trim();
+        description = sel('table').innerText.trim();
       }
     }
     // bright.com
     if (document.domain.indexOf('bright.com') != -1) {
-      if  (document.querySelector('.company-details') && document.querySelector('.job-details') && 
+      if  (sel('.company-details') && sel('.job-details') && 
         document.getElementById('job-description')) {
         isjobSite = true;
-        description = document.querySelector('.company-details').innerText.trim() + longLine +
-        document.querySelector('.job-details').innerText.trim() + longLine +
+        description = sel('.company-details').innerText.trim() + longLine +
+        sel('.job-details').innerText.trim() + longLine +
         document.getElementById('job-description').innerText.trim();
       }
     }
     // glassdoor.com
     if (document.domain.indexOf('glassdoor.com') != -1) {
-      if (document.querySelector('.desc')) {
+      if (sel('.desc')) {
         isjobSite = true;
-        description = document.querySelector('.desc').innerText.trim();
+        description = sel('.desc').innerText.trim();
       }
     }
     // linkedin.com
     if (document.domain.indexOf('linkedin.com') != -1) {
-      if (document.querySelector('.job-description') && document.querySelector('.job-skills') && 
-        document.querySelector('.company-description') && document.querySelector('.additional-info')) {
+      if (sel('.job-description') && sel('.job-skills') && 
+        sel('.company-description') && sel('.additional-info')) {
         isjobSite = true;
-        description = document.querySelector('.job-description').innerText.trim() + longLine +
-          document.querySelector('.job-skills').innerText.trim() + longLine +
-          document.querySelector('.company-description').innerText.trim() + longLine +
-          document.querySelector('.additional-info').innerText.trim();
+        description = sel('.job-description').innerText.trim() + longLine +
+          sel('.job-skills').innerText.trim() + longLine +
+          sel('.company-description').innerText.trim() + longLine +
+          sel('.additional-info').innerText.trim();
       }
-      if (document.querySelector('.job-description') && document.querySelector('.additional-info')) {
+      if (sel('.job-description') && sel('.additional-info')) {
         isjobSite = true;
-        description = document.querySelector('.job-description').innerText.trim() + longLine +
-          document.querySelector('.additional-info').innerText.trim();
+        description = sel('.job-description').innerText.trim() + longLine +
+          sel('.additional-info').innerText.trim();
       }
-      if (document.querySelector('.job-description') && document.querySelector('.company-description') 
-        && document.querySelector('.additional-info')) {
+      if (sel('.job-description') && sel('.company-description') 
+        && sel('.additional-info')) {
         isjobSite = true;
-        description = document.querySelector('.job-description').innerText.trim() + longLine +
-          document.querySelector('.company-description').innerText.trim() + longLine +
-          document.querySelector('.additional-info').innerText.trim();
+        description = sel('.job-description').innerText.trim() + longLine +
+          sel('.company-description').innerText.trim() + longLine +
+          sel('.additional-info').innerText.trim();
+      } 
+      if (sel('.job-desc .description-module')) {
+        isjobSite = true;
+        description = sel('.job-desc .top-card .content .title').innerText.trim() +'\n'+
+                      sel('.job-desc .top-card .content .sub-header').innerText.trim() +longLine+
+                      sel('.job-desc .description-module .content').innerText.trim() +longLine+
+                      sel('.job-desc .company-module .container-header').innerText.trim() + '\n'+
+                      sel('.job-desc .company-module .hiring-org-name').innerText.trim() +'\n'+
+                      sel('.job-desc .company-module .content').innerText.trim();
       }
     }
     // TheLadders.com
     if (document.domain.indexOf('theladders.com') != -1) {
-      if (document.querySelector('.shiftDown')) {
+      if (sel('.title') && sel('.description')) {
         isjobSite = true;
-        description = document.querySelector('.shiftDown').innerText.trim();
+        description = sel('.title').innerText.trim() +'\n'+
+                      sel('.company').innerText.trim() + sel('.location').innerText.trim()+longLine+
+                      sel('.description').innerText.trim();
       }
     }
     // snagajob.com
     if (document.domain.indexOf('snagajob.com') != -1) {
-      if (document.querySelector('.jobDescription')) {
+      if (sel('.jobDescription')) {
         isjobSite = true;
-        description = document.querySelector('.jobDescription').innerText.trim();
+        description = sel('.jobDescription').innerText.trim();
       }
     }
     // ziprecruiter.com
     if (document.domain.indexOf('ziprecruiter.com') != -1) {
-      if (document.querySelector('.jobDescription')) {
+      if (sel('.jobDescription')) {
         isjobSite = true;
-        description = document.querySelector('.jobDescription').innerText.trim();
+        description = sel('.jobDescription').innerText.trim();
       }
     }
     // dice.com
@@ -239,21 +250,33 @@ chrome.extension.onMessage.addListener(
         }
       } 
       if (document.location.href.indexOf('jobsearch/servlet') != -1) {
-        if (document.querySelector('.job_overview') && document.querySelector('.contact_info') &&
-          document.querySelector('.job_description')) {
+        if (sel('.job_overview') && sel('.contact_info') &&
+          sel('.job_description')) {
           isjobSite = true;
-          description = document.querySelector('.job_description').innerText.trim() + longLine +
-            document.querySelector('.job_overview').innerText.trim() + longLine +
-            document.querySelector('.contact_info').innerText.trim() + longLine;
+          description = sel('.job_description').innerText.trim() + longLine +
+            sel('.job_overview').innerText.trim() + longLine +
+            sel('.contact_info').innerText.trim() + longLine;
         }
       }
-
     }
+    // http://www.careerbuilder.com/
+    if (document.domain.indexOf('careerbuilder.c') != -1) {
+      if (sel('#job-title') && sel('#job-description-section')) {
+        isjobSite = true;
+        description = sel('#job-title').innerText.trim() + '\n'+
+                      sel('.header-byline').innerText.trim() + longLine +
+                      sel('#job-description-section').innerText.trim() +'\n'+
+                      sel('#job-requirements').innerText.trim() +'\n'+
+                      sel('#job-snapshot-section').innerText.trim() ;
+                      
+      }
+    }
+    
   if (!isjobSite) {
      // do extra magic to tell user we don't support this yet
-      document.querySelector('#appliedat-wrapper .header').setAttribute('style', 'border-width: 3px; border-color:red; border-bottom: 1px solid #C3D4DB');
-      document.querySelector('#appliedat-wrapper .main').setAttribute('style', 'border-width: 3px; border-color:red;');
-      document.querySelector('#appliedat-wrapper .footer').setAttribute('style', 'border-width: 3px; border-color:red; border-top: 1px solid #C3D4DB');
+      sel('#appliedat-wrapper .header').setAttribute('style', 'border-width: 3px; border-color:red; border-bottom: 1px solid #C3D4DB');
+      sel('#appliedat-wrapper .main').setAttribute('style', 'border-width: 3px; border-color:red;');
+      sel('#appliedat-wrapper .footer').setAttribute('style', 'border-width: 3px; border-color:red; border-top: 1px solid #C3D4DB');
       document.getElementById('appliedat_Description').style.color='red';
       description = "We don't support this site yet, so ENTER (copy and paste) the description on your own......"; 
     } 
